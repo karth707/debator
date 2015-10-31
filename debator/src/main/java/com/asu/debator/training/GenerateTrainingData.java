241f89d9129dd36e23a1434e1e56b46a65b48fd2
Kartheek Nallepalli <kartheek.nallepalli@gmail.com>
2015-10-30 19:38:27 -0700

    rule based classifiation test1

--- debator/src/main/java/com/asu/debator/training/GenerateTrainingData.java
+++ debator/src/main/java/com/asu/debator/training/GenerateTrainingData.java
@@ -4,8 +4,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +35,9 @@ public class GenerateTrainingData {
 			for(int index=0; index<inputSentences.size(); index++){
 				String sentence_left = (index==0) ? null : inputSentences.get(index-1);
 				String sentence = inputSentences.get(index);
+				if(sentence.split(" ").length < 3){
+					continue;
+				}
 				String sentence_right = (index==inputSentences.size()-1) ? null : inputSentences.get(index+1);
 				Annotation annotation = SubjectiveClassifier.classifySentence(sentence_left, sentence, sentence_right);
 				writer.write(sentence + DELIMITER + annotation.toString() + "\n");
@@ -42,8 +49,20 @@ public class GenerateTrainingData {
 	}
 	
 	public static void main(String[] args){
-		String inputPath = "";
-		String outputPath = "";
-		GenerateTrainingData.generate(inputPath, outputPath);
+		BasicConfigurator.configure();
+		org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
+		String outputPath = "/Users/KartheekGanesh/Desktop/NLPCorpora_Tagged/";
+		try {
+			Files.walk(Paths.get("/Users/KartheekGanesh/Desktop/NLPCorpora")).forEach(inputPath -> {
+			    if (Files.isRegularFile(inputPath)) {
+			    	logger.info("Processing: {}", inputPath.getFileName().toString());
+			    	GenerateTrainingData.generate(inputPath.toString(), outputPath 
+			    			+ inputPath.getFileName().toString().split(".txt")[0]
+			    			+ "_tagged.txt");
+			    }
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
